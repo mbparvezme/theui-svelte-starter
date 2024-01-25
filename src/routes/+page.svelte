2593313d9b7e339f8615a619772ea3667cdfec37
M B Parvez (Ron) <mbparvezme@gmail.com>
2024-01-26 00:14:03 +0600

    URL fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,10 @@
   <img src="/img/logo.webp" alt="TheUI Logo" class="w-16 mb-4">
   <h2 class="text-4xl font-light mb-4">THEUI-SVELTEKIT-STARTER</h2>
   <p class="flex gap-4 mb-auto text-sm">
-    <a class="decoration-wavy decoration-1 decoration-gray-300 decoration underline underline-offset-2" href="www.svelte.theui.dev/docs" target="_blank">READ THE DOCS</a>
+    <a class="decoration-wavy decoration-1 decoration-gray-300 decoration underline underline-offset-2" href="https://www.svelte.theui.dev/docs" target="_blank">READ THE DOCS</a>
     <a class="decoration-wavy decoration-1 decoration-gray-300 decoration underline underline-offset-2" href="https://github.com/mbparvezme/bnl-dashboard/issues" target="_blank">REPORT ISSUES</a>
   </p>
   <p>
-    <a class="text-xs" href="www.mbparvez.me" target="_blank">by M B Parvez</a>
+    <a class="text-xs" href="https://www.mbparvez.me" target="_blank">by M B Parvez</a>
   </p>
 </Container>
